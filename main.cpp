8d6a6b521e550bdfd1194e2e29d9262077137039
longmingfu <1032843260@qq.com>
2021-12-05 15:50:55 +0800

    123

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+using namespace std;
+/*
+
+*/
+
+int main(void)
+{
+	std::cout << "1.ÍøÕ¾404¹¥»÷" <<std::endl;
+	std::cout << "2.ÍøÕ¾´Û¸ÄÍøÒ³" << std::endl;
+	std::cout << "3.ÍøÕ¾¹¥»÷¼ÇÂ¼" << std::endl;
+	std::cout << "4.DNS¹¥»÷" << std::endl;
+	std::cout << "5.·þÎñÆ÷ÖØÆô¹¥»÷" << std::endl;
+
+	system("pause");
+
+	return 0;
+  
+}
+
+
+
